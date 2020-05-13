e65959949db107c695ca73ae6d15085380ac98ce
Patrick Roos <pa.roos@icloud.com>
2020-05-13 21:45:42 +0200

    Remove app.js

--- src/Routes.js
+++ src/Routes.js
@@ -4,14 +4,13 @@ import {
     BrowserRouter
   } from "react-router-dom";
 
-import App from './App';
 import View1 from './View1';
 import View2 from './View2';
 import View3 from './View3';
 
 const Root = () => (
         <BrowserRouter>
-            <Route exact path="/" component={App}/>
+            <Route exact path="/" component={View1}/>
             <Route exact path="/view-1" component={View1}/>
             <Route exact path="/view-2" component={View2}/>
             <Route exact path="/view-3" component={View3}/>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
-
-import history from './History';
 import Root from './Routes';
 
 ReactDOM.render(
